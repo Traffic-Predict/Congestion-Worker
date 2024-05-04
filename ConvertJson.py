b4d6bf6b99e56d7c1551ec059357f0dde797fae7
Heeville <jinuk0613@gmail.com>
2024-05-04 15:46:26 +0900

    [Add] ConvertJson.py, 도로 혼잡 정보

---
+++ ConvertJson.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+import sqlite3
+
+def determine_congestion(road_rank, speed):
+    congestion_levels = {
+        '101': {'congested': 40, 'slow': 80},  # 고속 도로
+        '102': {'congested': 30, 'slow': 60},  # 자동차 전용도로
+        '103': {'congested': 20, 'slow': 40},  # 일반 국도
+        '104': {'congested': 15, 'slow': 30}   # 시내 도로
+    }
+    if road_rank in congestion_levels:
+        if speed <= congestion_levels[road_rank]['congested']:
+            return 'congested'
+        elif speed <= congestion_levels[road_rank]['slow']:
+            return 'slow'
+        else:
+            return 'smooth'
+    return 'unknown'
+
+def get_db_connection():
+    conn = sqlite3.connect('daejeon_links.sqlite')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def convertData(data, start_id=1):
+    new_data = {"items": []}
+    current_id = start_id
+    conn = get_db_connection()
+    if "body" in data and "items" in data["body"]:
+        for item in data["body"]["items"]:
+            created_date = datetime.strptime(item["createdDate"], "%Y%m%d%H%M%S")
+            iso_date = created_date.isoformat() + "+09:00"  # KST 기준으로 +09:00 추가
+            link_id = int(item["linkId"])
+            db_query = conn.execute('SELECT link_id, road_name, road_rank FROM daejeon_link WHERE link_id = ?',
+                                    (link_id,))
+
+            link_info = db_query.fetchone()
+            if not link_info or link_info['road_rank'] in ('106', '107'): # 지방도는 실시간 교통 예측서 제외
+                continue
+            road_status = determine_congestion(link_info['road_rank'], float(item["speed"]))
+            converted_item = {
+                "id": current_id,
+                "speed": float(item["speed"]),
+                "road_status":road_status,
+                "date": iso_date,
+                #"time": datetime.strptime(item["createdDate"], "%Y%m%d%H%M%S").hour * 60 + int(
+                #    datetime.strptime(item["createdDate"], "%Y%m%d%H%M%S").minute / 10) * 10,
+                "link_Id": link_id,
+                "Node_Id": int(item["startNodeId"]),
+                "road_name": link_info["road_name"] if link_info else "Unknown",
+                "road_rank": link_info["road_rank"] if link_info else "Unknown",
+            }
+            new_data["items"].append(converted_item)
+            current_id += 1
+    conn.close()
+    return new_data
--- Workerflask.py
+++ Workerflask.py
@@ -1,20 +1,16 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import requests
-from datetime import datetime
-import sqlite3
 from dotenv import load_dotenv
+from ConvertJson import *
 import os
 
 app = Flask(__name__)
 
-# 환경 변수 설정 로드
 load_dotenv()
 
-# CORS 설정
 CORS(app, resources={r'*': {'origins': os.getenv("FE_ORIGIN")}})
 
-# 대전 지역 위경도 범위 정보
 '''
 대전 지역 대략적인 위경도 범위
        "minX": "127.269182",
@@ -22,12 +18,6 @@ CORS(app, resources={r'*': {'origins': os.getenv("FE_ORIGIN")}})
         "minY": "36.192478 ",
         "maxY": "36.497312",
 '''
-
-# 데이터베이스 연결 함수
-def get_db_connection():
-    conn = sqlite3.connect('daejeon_links.sqlite')
-    conn.row_factory = sqlite3.Row
-    return conn
 
 def callApi(minX, maxX, minY, maxY):
     api_url = "https://openapi.its.go.kr:9443/trafficInfo"
@@ -47,33 +37,6 @@ def callApi(minX, maxX, minY, maxY):
         return response.json()
     else:
         raise Exception(f"{response.status_code}")
-
-def convertData(data, start_id=1):
-    new_data = {"items": []}
-    current_id = start_id
-    conn = get_db_connection()
-    if "body" in data and "items" in data["body"]:
-        for item in data["body"]["items"]:
-            created_date = datetime.strptime(item["createdDate"], "%Y%m%d%H%M%S")
-            iso_date = created_date.isoformat() + "+09:00"  # KST 기준으로 +09:00 추가
-            link_id = int(item["linkId"])
-            db_query = conn.execute('SELECT link_id, road_name, road_rank, max_spd FROM daejeon_link WHERE link_id = ?', (link_id,))
-            link_info = db_query.fetchone()
-            converted_item = {
-                "id": current_id,
-                "speed": float(item["speed"]),
-                "date": iso_date,
-                "time": datetime.strptime(item["createdDate"], "%Y%m%d%H%M%S").hour * 60 + int(datetime.strptime(item["createdDate"], "%Y%m%d%H%M%S").minute /10)*10,
-                "link_Id": link_id,
-                "Node_Id": int(item["startNodeId"]),
-                "road_name": link_info["road_name"] if link_info else "Unknown",
-                "road_rank": link_info["road_rank"] if link_info else "Unknown",
-                "max_spd": link_info["max_spd"] if link_info else "Unknown"
-            }
-            new_data["items"].append(converted_item)
-            current_id += 1
-    conn.close()
-    return new_data
 
 @app.route('/main', methods=['POST'])
 def index():
